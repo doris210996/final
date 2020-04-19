a7b3cd2dacb9596cb1f5c1481df500ce573ae697
doronbruder <doron.bruder@mail.huji.ac.il>
2020-04-19 23:38:35 +0300

    with  the karnaval tests this time for real

---
+++ cmake-build-debug/tets/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /cs/usr/doronbruder/CLionProjects/final1/cmake-build-debug/tets/googletest-src
+# Build directory: /cs/usr/doronbruder/CLionProjects/final1/cmake-build-debug/tets/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
